5398ad83e470bef23415f985d391ef5eb6ae30e6
Jesus Valera Reales <github@jesusvalera.dev>
2026-06-05 11:07:58 +0200

    Update age from UserImportedTest
    Updated ages for several users in UserImporterTest.

--- tests/UserImporterTest.php
+++ tests/UserImporterTest.php
@@ -23,18 +23,18 @@ final class UserImporterTest extends TestCase
 *****************************************************************************************
 * ID		* COUNTRY	* NAME		* EMAIL				* AGE	*
 *****************************************************************************************
-* 200189617246	* Germany	* Lukas Schmidt	* lukas.shmidt@example.com	* 28	*
+* 200189617246	* Germany	* Lukas Schmidt	* lukas.shmidt@example.com	* 29	*
 * 200189016257	* Germany	* Maria Fischer	* maria.fischer@example.com	* 34	*
 * 230573109005	* Spain 	* Luis Sanchez	* luis.sanchez@example.com	* 24	*
-* 230854119034	* Italy 	* Elio Pausini	* elio.pausini@example.com	* 49	*
-* 270054311737	* India 	* Mitesh Kumari	* mitesh.kumari@example.com	* 28	*
-* 202160712259	* Germany	* Elena Mueller	* elena.muller@example.com	* 33	*
-* 270554319031	* India 	* Natasha Shah	* natasha.shah@example.com	* 36	*
-* 100000000000	* Australia	* Nevaeh Dunn	* nevaeh.dunn@example.com	* 54	*
-* 100000000001	* Norway	* Sara Abdallah	* sara.abdallah@example.com	* 80	*
+* 230854119034	* Italy 	* Elio Pausini	* elio.pausini@example.com	* 50	*
+* 270054311737	* India 	* Mitesh Kumari	* mitesh.kumari@example.com	* 29	*
+* 202160712259	* Germany	* Elena Mueller	* elena.muller@example.com	* 34	*
+* 270554319031	* India 	* Natasha Shah	* natasha.shah@example.com	* 37	*
+* 100000000000	* Australia	* Nevaeh Dunn	* nevaeh.dunn@example.com	* 55	*
+* 100000000001	* Norway	* Sara Abdallah	* sara.abdallah@example.com	* 81	*
 * 100000000002	* France	* Melvin Perrin	* melvin.perrin@example.com	* 61	*
 * 100000000003	* Australia	* Dawn Snyder	* dawn.snyder@example.com	* 73	*
-* 100000000004	* Netherlands	* Irina Kaptein	* irina.kaptein@example.com	* 40	*
+* 100000000004	* Netherlands	* Irina Kaptein	* irina.kaptein@example.com	* 41	*
 *****************************************************************************************
 12 users in total!
 
